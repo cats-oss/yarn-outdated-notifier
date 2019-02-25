9084926869bde6486390630e475fe48e2fd1ce84
TimFretwell <45966572+TimFretwell@users.noreply.github.com>
2019-02-25 14:46:33 +0000

    update to allow github enterprise addresses

--- bin/index.js
+++ bin/index.js
@@ -14,18 +14,19 @@ ${chalk.yellow('USAGE:')}
   $ outdated-notifier [options]
 
 ${chalk.yellow('OPTIONS:')}
-  --api-token       API token for GitHub (required)
-  --owner,      -o  repository owner name (default: "git config --get remote.origin.url" infomation)
-  --repository, -r  repository name (default: "git config --get remote.origin.url" infomation)
-  --title,      -t  issue title for GitHub
-  --labels,     -l  issue labels name for GitHub
-  --assignees,  -a  issue assignees name for GitHub
-  --excludes,   -e  path to yaml file which specify package names to exclude
-  --changelogs, -c  path to yaml file which specify changelog uris for the packages
-  --template        path to the template to use for notification
-  --dry-run         do not register on issue, output contents to stdout
-  --help,       -h  show help
-  --version,    -v  print the version
+  --api-token         API token for GitHub (required)
+  --githubApiAddress, provide address for github enterprise (default: api.github.com)
+  --owner,      -o    repository owner name (default: "git config --get remote.origin.url" infomation)
+  --repository, -r    repository name (default: "git config --get remote.origin.url" infomation)
+  --title,      -t    issue title for GitHub
+  --labels,     -l    issue labels name for GitHub
+  --assignees,  -a    issue assignees name for GitHub
+  --excludes,   -e    path to yaml file which specify package names to exclude
+  --changelogs, -c    path to yaml file which specify changelog uris for the packages
+  --template          path to the template to use for notification
+  --dry-run           do not register on issue, output contents to stdout
+  --help,       -h    show help
+  --version,    -v    print the version
 
 ${chalk.yellow('EXAMPLES:')}
   $ yarn outdated-notifier --api-token <your_personal_token>
